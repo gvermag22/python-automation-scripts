19729f5abf9e1cb988175a185a53f66482d3b2e9
gvermag22 <67405957+gvermag22@users.noreply.github.com>
2022-08-05 15:44:48 -0700

    Add files via upload

--- findcommon.sh
+++ findcommon.sh
@@ -9,6 +9,14 @@ cat $2 | cut -d',' -f1 | sort > $f2
 comm=${1}_${2}.comm
 comm -12 $f1 $f2 > $comm
 
-grep -f $comm $1
+remove=`cat $comm`
+grep -v "$remove" $1 > ${1}.withoutcommon
+grep -v "$remove" $2 > ${2}.withoutcommon
+
+grep -f $comm $1 
+grep -f $comm $1 | wc -l
+
+echo "Remove one of the files ${1}.withoutcommon or ${2}.withoutcommon as per your contact strategy"
+wc -l $1 ${1}.withoutcommon $2 ${2}.withoutcommon
 
 rm $f1 $f2 $comm
